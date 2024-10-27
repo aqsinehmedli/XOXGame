3fd95a964847f6fabd31fe106db433bbaab3f1c5
aqsinehmedli <aqsinhmdli03@gmail.com>
2024-10-27 21:12:06 +0400

    Add project files.

---
+++ XOXClient3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Client:");
+        var ip = IPAddress.Parse("192.168.0.101");
+        var port = 27001;
+        var client = new TcpClient();
+
+        try
+        {
+            client.Connect(ip, port);
+            using (var stream = client.GetStream())
+            {
+                Console.Write("Zehmet olmasa oyuncu adinizi daxil edin: ");
+                string playerName = Console.ReadLine();
+
+                byte[] nameData = Encoding.UTF8.GetBytes(playerName);
+                stream.Write(nameData, 0, nameData.Length);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string onlinePlayers = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine(onlinePlayers);
+
+                char playerSymbol = ' ';
+                while (playerSymbol != 'X' && playerSymbol != 'O')
+                {
+                    Console.Write("X mi yoxsa O olmaq istəyirsiniz? (X/O): ");
+                    var input = Console.ReadLine().ToUpper();
+
+                    if (input == "X" || input == "O")
+                    {
+                        playerSymbol = input[0];
+                        byte[] symbolData = Encoding.UTF8.GetBytes(playerSymbol.ToString());
+                        stream.Write(symbolData, 0, symbolData.Length);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ugursuz secim. Zəhmət olmasa 'X' veya 'O' daxil edin.");
+                    }
+                }
+
+                while (true)
+                {
+                    Console.Write("Rəqəmi daxil edin (1-9 arası): ");
+                    var input = Console.ReadLine();
+
+                    if (int.TryParse(input, out int num) && num >= 1 && num <= 9)
+                    {
+                        byte[] data = BitConverter.GetBytes(num);
+                        stream.Write(data, 0, data.Length);
+
+                        bytesRead = stream.Read(buffer, 0, buffer.Length);
+                        string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        Console.WriteLine("Serverdən gələn cavab: " + response);
+
+                        if (response.Contains("qazandı") || response.Contains("bərabərə"))
+                            break; 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ugursuz rəqəm daxil etdiniz. Zəhmət olmasa 1-9 arası bir rəqəm daxil edin.");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Xəta: " + ex.Message);
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
+
---
+++ XOXServer3/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static List<string> onlinePlayers = new List<string>();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Server:");
+        var ip = IPAddress.Parse("192.168.0.101");
+        var port = 27001;
+        var listener = new TcpListener(ip, port);
+        listener.Start();
+        Console.WriteLine("Server dinlənilir...");
+
+        while (true)
+        {
+            using (TcpClient client = listener.AcceptTcpClient())
+            using (var stream = client.GetStream())
+            {
+                // Oyunçu adını al
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string playerName = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"{playerName} qoşuldu.");
+
+                // Oyunçu adını qeyd et
+                onlinePlayers.Add(playerName);
+
+                // Oyunçu siyahısını göndər
+                SendOnlinePlayers(stream);
+
+                // Oyunçunun simvolunu seçmesini isteyin
+                char playerSymbol;
+                while (true)
+                {
+                    SendMessage(stream, "X mi yoxsa O olmaq istəyirsiniz? (X/O): ");
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string symbolChoice = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim().ToUpper();
+
+                    if (symbolChoice == "X" || symbolChoice == "O")
+                    {
+                        playerSymbol = symbolChoice[0];
+                        break;
+                    }
+                    else
+                    {
+                        SendMessage(stream, "Keçərsiz giriş. Zəhmət olmasa 'X' veya 'O' daxil edin.");
+                    }
+                }
+
+                char[,] board = new char[,] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
+                int moves = 0;
+
+                while (true)
+                {
+                    PrintBoard(board, stream);
+                    int number = ReceiveMove(stream);
+
+                    if (number >= 1 && number <= 9)
+                    {
+                        int row = (number - 1) / 3;
+                        int col = (number - 1) % 3;
+
+                        if (board[row, col] != 'X' && board[row, col] != 'O')
+                        {
+                            board[row, col] = playerSymbol; 
+                            moves++;
+
+                            if (CheckWin(board, playerSymbol))
+                            {
+                                PrintBoard(board, stream);
+                                string winMessage = $"{playerName} ({playerSymbol}) qalib geldi!";
+                                SendMessage(stream, winMessage);
+                                Console.WriteLine(winMessage);
+                                break;
+                            }
+                            else if (moves == 9)
+                            {
+                                PrintBoard(board, stream);
+                                SendMessage(stream, "Oyun bərabərədir!");
+                                Console.WriteLine("Oyun bərabərədir!");
+                                break;
+                            }
+                            // Değiştirme simvolu
+                            playerSymbol = (playerSymbol == 'X') ? 'O' : 'X';
+                        }
+                        else
+                        {
+                            SendMessage(stream, "Bu secim ugursuz, zəhmət olmasa başqa bir rəqəm seçin.");
+                        }
+                    }
+                    else
+                    {
+                        SendMessage(stream, "Ugursuz rəqəm, zəhmət olmasa 1-9 arası bir rəqəm girin.");
+                    }
+                }
+
+                // Bağlantını bağla
+                onlinePlayers.Remove(playerName);
+            }
+        }
+    }
+
+    static void SendOnlinePlayers(NetworkStream stream)
+    {
+        string playersList = "Online Oyunçular: " + string.Join(", ", onlinePlayers);
+        byte[] data = Encoding.UTF8.GetBytes(playersList);
+        stream.Write(data, 0, data.Length);
+    }
+
+    static void PrintBoard(char[,] board, NetworkStream stream)
+    {
+        StringBuilder sb = new StringBuilder("Yenilənmiş taxta:\n");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                sb.Append(board[i, j] + " ");
+            }
+            sb.AppendLine();
+        }
+        SendMessage(stream, sb.ToString());
+    }
+
+    static void SendMessage(NetworkStream stream, string message)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+    }
+
+    static int ReceiveMove(NetworkStream stream)
+    {
+        byte[] buffer = new byte[4];
+        stream.Read(buffer, 0, buffer.Length);
+        return BitConverter.ToInt32(buffer, 0);
+    }
+
+    static bool CheckWin(char[,] board, char player)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i, 0] == player && board[i, 1] == player && board[i, 2] == player)
+                return true;
+        }
+
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[0, j] == player && board[1, j] == player && board[2, j] == player)
+                return true;
+        }
+
+        if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
+            return true;
+        if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player)
+            return true;
+
+        return false;
+    }
+}
